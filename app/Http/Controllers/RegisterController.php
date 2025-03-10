7c2e3da9bcb7f088182b6424e571d63a5cacb729
pedrojaraujo <pedroa990@gmail.com>
2025-03-10 19:11:48 -0300

    Adiciona controlador de registro e rota de registro de usuário

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'user_type' => 'required|string|in:freelancer,contractor',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']), // Hash da senha
+            'user_type' => $data['user_type'],
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Usuário criado com sucesso!',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\RegisterController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::post('/register', [RegisterController::class, 'register']);
