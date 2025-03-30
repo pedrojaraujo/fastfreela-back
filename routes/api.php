819f4ebcd8da31546cf69e1dd0ab59ffb582f5c0
pedrojaraujo <peroa990@gmail.com>
2025-03-30 19:53:12 -0300

    Renomeia RegisterController para UserController e atualiza rotas correspondentes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
 //Login do usuário
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,7 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 //Registro do usuário
-Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/register', [UserController::class, 'register']);
 
 //Deletar usuário
-Route::middleware('auth:sanctum')->delete('/delete/{id}', [RegisterController::class, 'destroy']);
+Route::middleware('auth:sanctum')->delete('/delete/{id}', [UserController::class, 'destroy']);
